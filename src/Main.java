2f4ad8dce88d61cdb4ce2f4f8f77426ff973befd
Larisa test <alexandrova.uu@yandex.ru>
2023-06-14 09:24:32 +0300

    change wolf

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 15200;
+
+        int percent = 20;
+
+        int bonus = amount / percent * 1;
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
